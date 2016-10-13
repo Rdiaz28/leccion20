d0cac18960a866cde3e5b3cc3651a9196892934b
ruth diaz <rdiazmarchant@gmail.com>
2016-10-12 23:23:11 -0500

    ejercicio realizado y terminado

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // tu codigo va aca
+function callbackPromedio(a, b, c, callback) {
+	var promedio = (a+b+c)/3;
+	return callback(Promedio);
+}
+
+var segundo =1;
+
+function segundero(){
+	document.getElementById("titulo").innerHTML = segundo++;
+
+	return segundo;
+} 
+
+setInterval(segundero,1000);
+
